6ba0436c2be32e64f53db059a85777f42cc0f2c0
nbadin <nbadin011@gmail.com>
2022-06-17 20:37:23 +0300

    step 2

--- src/Run.php
+++ src/Run.php
@@ -13,10 +13,12 @@ Generate diff
 Usage:
   gendiff (-h|--help)
   gendiff (-v|--version)
+  gendiff [--format <fmt>] <firstFile> <secondFile>
 
 Options:
   -h --help                     Show this screen
   -v --version                  Show version
+  --format <fmt>                Report format [default: stylish]
 DOC;
 
 function run(): void
